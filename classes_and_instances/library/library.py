b0a719ab557fc36293f7f022f97abef7e8630afe
gst5 <georgidimitrov90@yahoobg>
2021-03-05 12:17:13 +0200

    upd w530

--- classes_and_instances/library/library.py
+++ classes_and_instances/library/library.py
@@ -8,7 +8,7 @@ class Library:
         if user not in self.user_records:
             self.user_records.append(user)
         else:
-            return f"User with id = {user} already registered in the library!"
+            return f"User with id = {user.user_id} already registered in the library!"
 
     def remove_user(self, user):
         if user in self.user_records:
@@ -18,6 +18,14 @@ class Library:
         else:
             return f'We could not find such user to remove!'
 
-
     def change_username(self, user_id, new_username):
-        pass
+        for user in self.user_records:
+            if user_id == user.user_id:
+                if user.username == new_username:
+                    return f'Please check again the provided username - it should be different than the username used so far!'
+                if user.username in self.rented_books:
+                    self.rented_books[user.username] = self.rented_books[new_username]
+                    del self.rented_books[user.username]
+                user.username = new_username
+                return f'Username successfully changed to: {new_username} for userid: {user_id}'
+        return f'There is no user with id = {user_id}!'
