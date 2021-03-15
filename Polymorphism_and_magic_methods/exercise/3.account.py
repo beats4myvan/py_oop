2f0c31e8c490c51c9afb2345ddb76321963f8265
gst5 <georgidimitrov90@yahoobg>
2021-03-15 16:27:40 +0200

    polymocrphism exe

--- Polymorphism_and_magic_methods/exercise/3.account.py
+++ Polymorphism_and_magic_methods/exercise/3.account.py
@@ -1,25 +1,55 @@
-from abc import ABC
-
-
 class Account:
     def __init__(self, owner, amount=0):
         self.owner = owner
         self.amount = amount
         self._transactions = []
 
+    def __add__(self, other):
+        account = Account(f'{self.owner}&{other.owner}', self.amount + other.amount)
+        account._transactions = self._transactions + other._transactions
+        return account
+
+    def __str__(self):
+        return f"{__class__.__name__} of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self):
+        return f"{__class__.__name__}({self.owner}, {self.amount})"
+
+    def __len__(self):
+        return len(self._transactions)
+
+    def __getitem__(self, index):
+        return self._transactions[index]
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __ge__(self, other):
+        return self.balance >= other.balance
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
     def add_transaction(self, amount):
-        pass
+        if not isinstance(amount, int):
+            raise ValueError('please use int for amount')
+        self._transactions.append(amount)
 
+    @property
     def balance(self):
-        pass
+        return self.amount + sum(self._transactions)
 
-    def validate_transaction(self, account: Account, amount_to_add):
-        pass
+    @staticmethod
+    def validate_transaction(account, amount_to_add):
+        if account.amount + amount_to_add < 0:
+            raise ValueError("sorry cannot go in debt!")
+        account._transactions.append(amount_to_add)
+        return f"New balance: {account.balance}"
+
 
 acc = Account('bob', 10)
 acc2 = Account('john')
 print(acc)
-print(repr(acc))
 acc.add_transaction(20)
 acc.add_transaction(-20)
 acc.add_transaction(30)
